5b582fcbdd1f4765e53645debfa5f6e97b66afbd
brendon <brendon@localhost.localdomain>
2021-04-11 08:12:48 -0600

    Fixed fixed request error handling.

--- youtube_search_scraper.py
+++ youtube_search_scraper.py
@@ -2,6 +2,7 @@ import asyncio
 import threading
 
 import pyppeteer
+from pyppeteer.errors import TimeoutError
 from requests_html import HTML, AsyncHTMLSession
 from typing import List, Tuple
 from urllib.parse import quote_plus
@@ -11,6 +12,8 @@ SIMPLE_SEARCH_RESULT_CLASS_FINGERPRINT = ".yt-simple-endpoint.style-scope.ytd-vi
 FALLBACK_SEARCH_RESULT_CLASS_FINGERPRINT = ".yt-simple-endpoint.style-scope.ytd-rich-grid-media"
 
 SEARCH_BASE = "https://www.youtube.com/results?search_query="
+
+RENDER_TIMEOUT_SECS = 10
 
 
 # Because attempting to handle signals on threads other than the main is an error, and the server is multithreaded.
@@ -31,12 +34,15 @@ async def get_search_results_async(query: str) -> List[Tuple[str, str]]:
 
         response = await session.get(SEARCH_BASE + encoded_query)
         html: HTML = response.html
-        await html.arender()
+        await html.arender(timeout=RENDER_TIMEOUT_SECS)
         attrs = (link.attrs for link in html.find(SIMPLE_SEARCH_RESULT_CLASS_FINGERPRINT))
 
         # TODO: Fallback if attrs was empty
         pairs = ((attr['title'], attr['href']) for attr in attrs)  # Extract the title and hrefs
         return [(title, href.split("=")[-1]) for title, href in pairs]  # Get the video codes out of the pairs
+    except TimeoutError:
+        # TODO: Log
+        return []
     finally:
         if session:
             await session.close()
